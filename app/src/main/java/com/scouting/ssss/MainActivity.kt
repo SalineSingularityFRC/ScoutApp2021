9378c900935e0085988bd61df53d44634c2f7efe
Skylar Bleed <monarrk@aaathats3as.com>
2020-06-30 21:44:16 -0400

    Begin our port

---
+++ app/src/main/java/com/scouting/ssss/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.scouting.ssss
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+
+// The beginning screen
+class MainActivity : AppCompatActivity() {
+    // Logging tag
+    private val tag = "7G7 Bluetooth"
+    private var started = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Create buttons
+        val newMatch = findViewById<Button>(R.id.newMatchBtn)
+
+        // Set behavior for the button
+        newMatch.setOnClickListener {
+            Log.i(tag, "Clicked new match button")
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(tag, "Started main activity")
+
+        // bluetooth.setup
+        if (!started) {
+            // bluetooth.send("")
+            started = true
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(tag, "Destroyed the main activity")
+        // bluetooth.end
+    }
+}
