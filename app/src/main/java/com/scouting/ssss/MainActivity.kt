a269967ca7e865c729bb5231e4393a77fdeec13a
skye bleed <monarrk@aaathats3as.com>
2020-12-14 13:43:29 -0500

    more improvements still to the setvers script

--- app/src/main/java/com/scouting/ssss/MainActivity.kt
+++ app/src/main/java/com/scouting/ssss/MainActivity.kt
@@ -10,10 +10,16 @@ import androidx.appcompat.app.AppCompatActivity
 // the version
 // NOTE(@monarrk): this is set with a special compiler script (scripts/setvers.sh)
 // this is ONLY usable on a unix (linux, mac, bsd, etc) computer with bourne shell (usually /bin/sh)
+// or on windows with git bash or windows subsystem for linux
+//
 // this works by using sed to "s/{{VERSION}}/${VERSION}/g"
 // where $VERSION is the content of version.txt and the current short git hash (`git rev-parse --short HEAD`)
+//
 // effectively this means any instance of the text {{VERSION}} in this file will be replaced with the version
 // this allows me to increment/set the version easily with, say, every git commit so we know what's running on each tablet
+//
+// for more implementation details, the script is pretty well commented and should be ok to understand
+//
 // this was written by skye bleed and is largely magic
 // if she is no longer working on this codebase, feel free to remove or modify it
 const val VERSION = "{{VERSION}}"
--- scripts/setvers.sh
+++ scripts/setvers.sh
@@ -1,14 +1,37 @@
 #!/bin/sh
 
+# argument parsing
+case "$1" in
+  "install") ACTION="./gradlew installDebug" ;; # install the app
+  *) ACTION="./gradlew" ;; # otherwise just build
+esac
+
+# if $ACTION is somehow still unset, set it
+[ -z "$ACTION" ] && ACTION="./gradlew"
+
+# the file we're searching
 FILE="app/src/main/java/com/scouting/ssss/MainActivity.kt"
+
+# get the version from version.txt
+# if it fails, exit
 VERSION=`cat version.txt || exit 1`
+
+# get the short git hash. something like "a5ad81d"
 HASH=`git rev-parse --short HEAD`
+
+# concatenate the version and the hash, something like "1.0.0-a5ad81d"
 FULL="${VERSION}-${HASH}"
 
+# get the initial contents of the file from sed
+# capture this instead of echoing directly to the file to avoid a data race
 FIRST=`cat ${FILE} | sed "s/{{VERSION}}/${FULL}/g"`
+# write the updated file
 echo "${FIRST}" > "${FILE}"
 
-./gradlew
+# build
+${ACTION}
 
+# re-insert the text {{VERSION}} for next time, again capturing rather than echoing
 SECOND=`cat app/src/main/java/com/scouting/ssss/MainActivity.kt | sed "s/${FULL}/{{VERSION}}/g"`
+# write the un-updated file
 echo "${SECOND}" > "${FILE}"
