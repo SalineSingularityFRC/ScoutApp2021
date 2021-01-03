0778b3b20136b8ffbc9cced9234f3134c1cfd035
Skye Bleed <skye@studio.local>
2021-01-03 16:03:23 -0500

    Correctly place listview

--- app/src/main/java/com/scouting/scoutapp/Teams.kt
+++ app/src/main/java/com/scouting/scoutapp/Teams.kt
@@ -1,6 +1,5 @@
 package com.scouting.scoutapp
 
-import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
@@ -16,7 +15,7 @@ class Teams : AppCompatActivity() {
         Log.i(tag, "Started Teams activity")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_teams)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        //setSupportActionBar(findViewById(R.id.toolbar))
 
         // get the view with its id in the layout
         this.view = findViewById(R.id.list_item)
@@ -31,30 +30,9 @@ class Teams : AppCompatActivity() {
         val list = listOf<String>("a", "b", "c", "d", "e", "f", "g")
 
         // create an adapter attached to a layout, NOT a fragment or activity
+        // must use the constructor that takes in a TextView (list_text in this instance)
         val arrayAdapter = ArrayAdapter<String>(this, R.layout.listlayout, R.id.list_text, list)
 
         view.adapter = arrayAdapter
-
-        // make sure view isn't null
-        // this if pattern unwraps the ListView? into a non-optional type (ListView)
-        /*if (view != null) {
-            // set the adapter to the list
-            view.adapter = arrayAdapter
-        } else {
-            // let our users know and exit the app
-            Log.e(tag, "View was null!")
-
-            // make a popup message to alert the user
-            // the "alert" function is defined in Util.kt
-            alert(
-                this,
-                "FATAL LAYOUT ERROR!",
-                "FATAL ERROR: Teams.kt: view (type ListView?) is null. Please contact the scouting team to alert them of the error BEFORE you restart the app.",
-                DialogInterface.OnClickListener {
-                    // TODO: attempt to recover?
-                    // dialog, id
-                    _, _ -> Log.i(tag, "Attempting to recover...")
-                })
-        }*/
     }
 }
